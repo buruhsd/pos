955c351439cd29a4bd49a8dd37904403b4a0d012
buruh <buruhsd@outlook.co.id>
2018-12-16 14:44:18 +0700

    test

--- resources/views/konten/backend/produk/cetak_barcode/index.blade.php
+++ resources/views/konten/backend/produk/cetak_barcode/index.blade.php
@@ -18,8 +18,10 @@
 </head><body>
 	@for($i=1;$i<=$jml;$i++)
 			@if(!empty($produk->barcode))
+			{{$produk->barcode}}
  					<img class="barcode_produk" src="data:image/png;base64,{!! DNS1D::getBarcodePNG($produk->barcode, 'C128') !!}" alt="barcode"/>
  			@else
+ 			{{$produk->sku}}
  					<img class="barcode_produk" src="data:image/png;base64,{!! DNS1D::getBarcodePNG($produk->sku, 'C128') !!}" alt="barcode"/>
 			@endif
 	@endfor
