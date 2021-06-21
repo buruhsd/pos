f5842ac4d48e8b836a5913fcf9d361e85ae2c532
muhammad al jawad <buruhsd@outlook.co.id>
2021-06-21 16:43:46 +0700

    Update UserLevelSeeder.php

--- database/seeds/UserLevelSeeder.php
+++ database/seeds/UserLevelSeeder.php
@@ -14,21 +14,21 @@ class UserLevelSeeder extends Seeder
     {
 
     	// insert level administrator
-        $check = UserLevel::where('id', '=', 1)->first();
-        if(count($check)<=0){
+        //$check = UserLevel::where('id', '=', 1)->first();
+        //if($check == null){
         	UserLevel::create(['id' => 1, 'nama'	=> 'administrator']);
-        }
+        //}
 
     	// insert level karyawan
-        $check = UserLevel::where('id', '=', 2)->first();
-        if(count($check)<=0){
+        //$check = UserLevel::where('id', '=', 2)->first();
+        //if(count($check)<=0){
         	UserLevel::create(['id' => 2, 'nama'	=> 'karyawan']);
-        }
+        //}
 
-        $check = UserLevel::where('id', '=', 3)->first();
-        if(count($check)<=0){
+        //$check = UserLevel::where('id', '=', 3)->first();
+        //if(count($check)<=0){
             UserLevel::create(['id' => 3, 'nama'    => 'member']);
-        }
+        //}
 
         
         $u_obj = app('App\Repositories\Contracts\Mst\UserRepoInterface');
