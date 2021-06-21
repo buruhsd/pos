50899b99c22d00c92ba5312fd76c89e75f13fd2c
muhammad al jawad <buruhsd@outlook.co.id>
2021-06-21 16:46:11 +0700

    Update setupVariableSeeder.php

--- database/seeds/setupVariableSeeder.php
+++ database/seeds/setupVariableSeeder.php
@@ -14,25 +14,25 @@ class setupVariableSeeder extends Seeder
     {
 
     	// nama aplikasi
-        $sv = SetupVariable::where('variable', '=', 'nama_aplikasi')->first();
-        if(count($sv)<=0){
+        //$sv = SetupVariable::where('variable', '=', 'nama_aplikasi')->first();
+        //if(count($sv)<=0){
         	$data = ['variable' => 'nama_aplikasi', 'value' => 'Sistem POS'];
         	SetupVariable::create($data);
-        }
+        //}
  
         // config backup
-        $sv = SetupVariable::where('variable', '=', 'backup_db')->first();
-        if(count($sv)<=0){
+        //$sv = SetupVariable::where('variable', '=', 'backup_db')->first();
+        //if(count($sv)<=0){
             $data = ['variable' => 'backup_db', 'value' => '1']; // 1/0
             SetupVariable::create($data);
-        }
+        //}
  
         // config backup
-        $sv = SetupVariable::where('variable', '=', 'jam_backup')->first();
-        if(count($sv)<=0){
+       // $sv = SetupVariable::where('variable', '=', 'jam_backup')->first();
+        //if(count($sv)<=0){
             $data = ['variable' => 'jam_backup', 'value' => '11:00'];  
             SetupVariable::create($data);
-        }
+        //}
  
 
 
